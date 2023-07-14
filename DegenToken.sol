e4ec6788848dfb5125d4f4ad145e6f64b47f06b0
harsh08hc <136369712+harsh08hc@users.noreply.github.com>
2023-07-14 11:14:21 +0530

    DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,44 +1,60 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
-contract DegenGamingToken is ERC20 {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
-    // Mint new tokens
+    constructor() ERC20("Degen", "DGN") {}
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    // Transfer tokens
-    function transfer(address to, uint256 amount) public returns (bool) {
-        return _transfer(msg.sender, to, amount);
+    function transferTokens(address _reciever, uint amount) external {
+        require(balanceOf(msg.sender) >= amount, "you are not owner");
+        approve(msg.sender, amount);
+        transferFrom(msg.sender, _reciever, amount);
     }
 
-    // Redeem tokens for items
-    function redeem(uint256 amount) public {
+    function checkBalance() external view returns(uint) {
+        return balanceOf(msg.sender);
+    }
+
+    function burnTokens(uint amount) external {
+        require(balanceOf(msg.sender) >= amount, "You do not have enough Tokens");
         _burn(msg.sender, amount);
     }
 
-    // Check token balance
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
+    function gameStore() public pure returns(string memory) {
+        return "1. ProPlayer NFT value = 200\n2. SuperNinja value = 100\n3. DegenCap value = 75";
     }
 
-    // Burn tokens
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
+    function redeemTokens(uint choice) external payable {
+        require(choice <= 3, "Invalid selection.");
+
+        // Redeem a ProPlayer NFT.
+        if (choice == 1) {
+            require(balanceOf(msg.sender) >= 200, "Insufficient balance.");
+            approve(msg.sender, 200);
+            transferFrom(msg.sender, owner(), 200);
+        }
+
+        // Redeem a SuperNinja NFT.
+        else if (choice == 2) {
+            require(balanceOf(msg.sender) >= 100, "Insufficient balance.");
+            approve(msg.sender, 100);
+            transferFrom(msg.sender, owner(), 100);
+        }
+
+        // Redeem a DegenCap NFT.
+        else {
+            require(balanceOf(msg.sender) >= 75, "Insufficient balance.");
+            approve(msg.sender, 75);
+            transferFrom(msg.sender, owner(), 75);
+        }
     }
-
-    address private owner;
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can mint tokens");
-        _;
-    }
-
-    function setOwner(address newOwner) public {
-        require(msg.sender == owner, "Only the owner can change the owner");
-        owner = newOwner;
-    }
-
 }
